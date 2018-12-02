771d822540f6f339e42cfbe77747ef955c634d14
Alex A <ama291@gmail.com>
2018-12-02 00:09:44 -0600

    summary

--- game/src/Main.java
+++ game/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         menu.setResizable(false);
         Container contentPane = menu.getContentPane();
-        JLabel actualInstructions = new JLabel("<html> <p style=\"padding-left: 260; text-align: center;\"> <br><br><br><br><br> *** INSTRUCTIONS *** <br><br><br> Press I to view your current inventory. <br> Walk onto the store or the market, and press Enter to bring up the interface.<br> Press Space to advance time. <br> Select a button below to begin. <br><br><br><br></p></html>");
+        JLabel actualInstructions = new JLabel("<html> <p style=\"text-align: center;\"> *** Energy Farm ***<br><br><br> Energy Farm is a strategy game where you own a property designed to generate and sell energy in an environmentally friendly way, with the goal of maximizing profits. You start with $100,000 in initial capital and have the ability to buy equipment to produce energy. You can then sell the energy you produce on the market for the fair market price determined by the game’s current economy. Throughout time, your goal is to make profit-maximizing decisions in order to end up with the greatest amount of profit possible. <br><br><br>*** INSTRUCTIONS *** <br><br><br> Press I to view your current inventory. <br> Walk onto the store or the market, and press Enter to bring up the interface.<br> Press Space to advance time. <br> Select a button below to begin. <br><br><br><br></p></html>");
         actualInstructions.setAlignmentX(Component.CENTER_ALIGNMENT);
         // Create Levels
         JButton l1Button  = new JButton("Novice");

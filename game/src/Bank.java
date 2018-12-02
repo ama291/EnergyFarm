67c5e68849b72d7665b58c9bddc7bacec21041ea
jigar <jigshahpuzzle@gmail.com>
2018-12-02 13:51:00 -0600

    Added bank

---
+++ game/src/Bank.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Bank {
+
+    //Attributes
+    private int maturity;
+    private double interestRate;
+    private double[] debtPayments;
+
+    //Constructors
+    public Bank(double startRate, int debtMaturity, int gameLen) {
+        interestRate = startRate;
+        debtPayments = new double[gameLen];
+        maturity = debtMaturity;
+        Arrays.fill(debtPayments, 0);
+    }
+
+    //Methods
+    public double getRate() { return interestRate; }
+
+    public void computePayment(double borrowed) {
+        return borrowed + (borrowed * interestRate);
+    }
+
+    public void addPayment(int year, double amount) {
+        debtPayments[year] += amount;
+    }
+
+    public double getPayment(int year) {
+        return debtPayments[year];
+    }
+
+    public void updateRate() {
+        Random r = new Random();
+        double returns = r.nextGaussian() * 0.005;
+        interestRate += returns;
+    }
+
+}

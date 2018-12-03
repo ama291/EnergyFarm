a1203482889f051216e4d0529c82dff304cb39b4
jigar <jigshahpuzzle@gmail.com>
2018-12-02 20:03:41 -0600

    remove print

--- game/src/FullGame.java
+++ game/src/FullGame.java
@@ -481,7 +481,6 @@ public class FullGame extends Game {
     }
 
     public void advance() {
-        System.out.println("*** IN ADVANCE ***");
         if (!gameOver) {
             for (Equipment e : player.getInventory()) {
                 //TODO randomize this

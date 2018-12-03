b54a1d8ee099d0c314469a0fb49cea97f80e8dcf
jigar <jigshahpuzzle@gmail.com>
2018-12-02 19:33:13 -0600

    better game params

--- game/src/Level3.java
+++ game/src/Level3.java
@@ -3,7 +3,7 @@ public class Level3 extends FullGame {
     public Level3() {
         super(3,1000, 60);
         this.store.setPrice(50000, 40000,300000);
-        this.store.setProduction(500, 500, 3000);
+        this.store.setProduction(250, 250, 1500);
         this.store.setStd(0.05, 0.1, 0.2);
         this.store.clearInventory();
         this.store.generateInventory("wind");
